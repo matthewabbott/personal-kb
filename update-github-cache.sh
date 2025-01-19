97565186e6ee2ac73e89440bedf51a5d4f8c99e3
matthewabbott <ttobbatam@gmail.com>
2025-01-19 08:55:49 +0000

    Add logging to cache update script

--- update-github-cache.sh
+++ update-github-cache.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Logging
+exec 1>> /var/log/github-cache.log 2>&1
+echo "$(date): Starting GitHub cache update"
+
 SITE_DIR="/var/www/html/personal-kb"
 DATA_DIR="$SITE_DIR/data"
 
@@ -12,6 +16,8 @@ curl -H "Accept: application/vnd.github.v3+json" \
      | jq 'sort_by(.pushed_at) | reverse' \
      > "$DATA_DIR/repos.json"
 
-# add with
-# crontab -e
-# 0 * * * * /root/personal-kb/update-github-cache.sh
+echo "$(date): Finished GitHub cache update"
+
+# Add with:
+#   crontab -e
+#   0 * * * * /root/personal-kb/update-github-cache.sh
